2a4b5c52d27398865869b3a75367b719447a1f15
leixus <1442526564@qq.com>
2019-12-17 20:32:56 +0800

    http详解

--- Angularjs/learn-angularjs/app/view1/view1.js
+++ Angularjs/learn-angularjs/app/view1/view1.js
@@ -9,6 +9,22 @@ angular.module('myApp.view1', ['ngRoute'])
         });
     }])
 
-    .controller('View1Ctrl', ['$scope', function($scope) {
+    .controller('View1Ctrl', ['$scope', '$log', '$http', function($scope, $log, $http) {
         $scope.testView1 = '123456';
+
+        $scope.dataJson = [];
+
+        $http({
+            method: 'POST',
+            url: 'https://api.apiopen.top/getWangYiNews',
+            data: {
+                page: 1,
+                count: 10
+            }
+        }).then(function successCallback(response) {
+            $log.log(response);
+            $scope.dataJson = response.data.result;
+        }, function errorCallback(response) {
+            // 请求失败执行代码
+        });
     }]);
